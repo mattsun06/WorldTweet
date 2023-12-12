b30a84b87c660eb7c085f3d2120d754b1b6a60f4
mattsun06 <matsukei.tech@gmail.com>
2023-12-12 21:48:51 +0900

    未ログイン状態はindexアクションにリダイレクト

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @tweets = Tweet.all
@@ -39,5 +40,11 @@ class TweetsController < ApplicationController
   
   def tweet_params
     params.require(:tweet).permit(:name, :text, :image)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
